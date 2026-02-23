b871554062fc39ae0c893f6ee399bc6ee37cebc3
Võ Lê Gia Bảo <baovo.280905@gmail.com>
2026-02-23 10:17:34 +0700

    Update Bai1.java

--- Lab2_TuHoc/src/Bai1.java
+++ Lab2_TuHoc/src/Bai1.java
@@ -17,14 +17,32 @@ public class Bai1 {
             if(arr[i] > max) max = arr[i];
             if(arr[i] % 2 ==0) count ++;
         }
+        System.out.println("\nTổng phần tử của mảng là " + sum);
+        System.out.println("Phần tử lớn nhất của mảng là " + max);
+        System.out.println("Số phần tử chẵn trong mảng là "+count);
+
         System.out.println("Mảng sau khi đảo chiều");
         System.out.print("[");
         for(int j = n-1; j>=0;j--){
             System.out.print(arr[j]+",");
         }
-        System.out.print("]");
-        System.out.println("\nTổng phần tử của mảng là " + sum);
-        System.out.println("Phần tử lớn nhất của mảng là " + max);
-        System.out.println("Số phần tử chẵn trong mảng là "+count);
+        System.out.println("]");
+        //Sắp xếp lại mảng(bubble sort)
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Mảng sau khi sắp xếp:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+
+
+    }
     }
 }
+
