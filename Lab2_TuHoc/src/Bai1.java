5164f141af25bc644434864880f42270a2b27c98
Võ Lê Gia Bảo <baovo.280905@gmail.com>
2026-02-23 10:05:47 +0700

    Update bai2

--- Lab2_TuHoc/src/Bai1.java
+++ Lab2_TuHoc/src/Bai1.java
@@ -8,12 +8,23 @@ public class Bai1 {
         int n = sc.nextInt();
         int[] arr = new int[n];
         int sum = 0;
-
+        int max = 0;
+        int count = 0;
         for(int i =0;i<n;i++){
-            System.out.print("Nhập phần tử thứ " + i + ":");
+            System.out.print("Nhập phần tử thứ " + i + ": ");
             arr[i] = sc.nextInt();
             sum += arr[i];
+            if(arr[i] > max) max = arr[i];
+            if(arr[i] % 2 ==0) count ++;
         }
-        System.out.print("Tổng phần tử của mảng là " + sum);
+        System.out.println("Mảng sau khi đảo chiều");
+        System.out.print("[");
+        for(int j = n-1; j>=0;j--){
+            System.out.print(arr[j]+",");
+        }
+        System.out.print("]");
+        System.out.println("\nTổng phần tử của mảng là " + sum);
+        System.out.println("Phần tử lớn nhất của mảng là " + max);
+        System.out.println("Số phần tử chẵn trong mảng là "+count);
     }
 }
